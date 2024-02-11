371dbb876e8fd0395df3fc1f4ee24ab1831745f7
Dilshad khan <152077382+imdilshad@users.noreply.github.com>
2024-02-12 02:30:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let img_sec=document.getElementById("img-sec");
+
+let box1=`<img src='/public/images/box1.webp' />`;
+let box2=`<img src='/public/images/box2.webp' />`;
+let box3=`<img src='/public/images/box3.webp' />`;
+let box4=`<img src='/public/images/box4.webp' />`;
+let box5=`<img src='/public/images/box5' />`;
+let box6=`<img src='/public/images/box6.webp' />`;
+let box7=`<img src='/public/images/box7.webp' />`;
+let box8=`<img src='/public/images/box8.webp' />`;
+
